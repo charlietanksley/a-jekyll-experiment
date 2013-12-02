c201ec9f2921ffc5bddb82ad98837aa669d65091
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-12-02 15:09:36 -0500

    just getting something basic going with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', []);
+
+};
